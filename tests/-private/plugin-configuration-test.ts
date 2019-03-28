56e6232ea1c3bd3b92d2a55727497ad1d1653e11
Dan Freeman <dfreeman@salsify.com>
2019-03-28 22:22:31 +0100

    Accept plugin arrays directly as configuration targets.

--- tests/-private/plugin-configuration-test.ts
+++ tests/-private/plugin-configuration-test.ts
@@ -1,32 +1,25 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { findPluginIndex } from '../../src/-private/plugin-configuration';
-import { ConfigurationTarget } from '../../src';
 
 describe('Utilities | plugin-configuration', () => {
   describe('findPluginIndex', () => {
-    let target: ConfigurationTarget = {
-      options: {
-        babel: {
-          plugins: ['short-name', 'babel-plugin-long-name', '/path/to/node_modules/babel-plugin-full-path/index.js']
-        }
-      }
-    };
+    let plugins = ['short-name', 'babel-plugin-long-name', '/path/to/node_modules/babel-plugin-full-path/index.js'];
 
     it('correctly returns -1', () => {
-      expect(findPluginIndex({}, 'foo-bar')).to.equal(-1);
+      expect(findPluginIndex([], 'foo-bar')).to.equal(-1);
     });
 
     it('locates plugins when requested by short name', () => {
-      expect(findPluginIndex(target, 'short-name')).to.equal(0);
-      expect(findPluginIndex(target, 'long-name')).to.equal(1);
-      expect(findPluginIndex(target, 'full-path')).to.equal(2);
+      expect(findPluginIndex(plugins, 'short-name')).to.equal(0);
+      expect(findPluginIndex(plugins, 'long-name')).to.equal(1);
+      expect(findPluginIndex(plugins, 'full-path')).to.equal(2);
     });
 
     it('locates plugins when requested by normalized name', () => {
-      expect(findPluginIndex(target, 'babel-plugin-short-name')).to.equal(0);
-      expect(findPluginIndex(target, 'babel-plugin-long-name')).to.equal(1);
-      expect(findPluginIndex(target, 'babel-plugin-full-path')).to.equal(2);
+      expect(findPluginIndex(plugins, 'babel-plugin-short-name')).to.equal(0);
+      expect(findPluginIndex(plugins, 'babel-plugin-long-name')).to.equal(1);
+      expect(findPluginIndex(plugins, 'babel-plugin-full-path')).to.equal(2);
     });
   });
 });
--- tests/index-test.ts
+++ tests/index-test.ts
@@ -13,31 +13,25 @@ describe('Public Helpers', () => {
       'C:\\path\\to\\node_modules\\@scope\\babel-plugin-scoped-windows-path\\lib\\plugin.js'
     ];
 
-    let target = {
-      options: {
-        babel: {
-          plugins: [shortNamePlugin, normalizedNamePlugin, fullPathPlugin, scopedPathPlugin, scopedWindowsPathPlugin]
-        }
-      }
-    };
+    let config = [shortNamePlugin, normalizedNamePlugin, fullPathPlugin, scopedPathPlugin, scopedWindowsPathPlugin];
 
     it('locates plugins by short and normalized names', () => {
-      expect(hasPlugin(target, 'nonexistent')).to.be.false;
+      expect(hasPlugin(config, 'nonexistent')).to.be.false;
 
-      expect(hasPlugin(target, 'short-name')).to.be.true;
-      expect(hasPlugin(target, 'babel-plugin-short-name')).to.be.true;
+      expect(hasPlugin(config, 'short-name')).to.be.true;
+      expect(hasPlugin(config, 'babel-plugin-short-name')).to.be.true;
 
-      expect(hasPlugin(target, 'long-name')).to.be.true;
-      expect(hasPlugin(target, 'babel-plugin-long-name')).to.be.true;
+      expect(hasPlugin(config, 'long-name')).to.be.true;
+      expect(hasPlugin(config, 'babel-plugin-long-name')).to.be.true;
 
-      expect(hasPlugin(target, 'full-path')).to.be.true;
-      expect(hasPlugin(target, 'babel-plugin-full-path')).to.be.true;
+      expect(hasPlugin(config, 'full-path')).to.be.true;
+      expect(hasPlugin(config, 'babel-plugin-full-path')).to.be.true;
 
-      expect(hasPlugin(target, '@scope/scoped-path')).to.be.true;
-      expect(hasPlugin(target, '@scope/babel-plugin-scoped-path')).to.be.true;
+      expect(hasPlugin(config, '@scope/scoped-path')).to.be.true;
+      expect(hasPlugin(config, '@scope/babel-plugin-scoped-path')).to.be.true;
 
-      expect(hasPlugin(target, '@scope/scoped-windows-path')).to.be.true;
-      expect(hasPlugin(target, '@scope/babel-plugin-scoped-path')).to.be.true;
+      expect(hasPlugin(config, '@scope/scoped-windows-path')).to.be.true;
+      expect(hasPlugin(config, '@scope/babel-plugin-scoped-path')).to.be.true;
     });
   });
 
@@ -50,117 +44,103 @@ describe('Public Helpers', () => {
       'C:\\path\\to\\node_modules\\@scope\\babel-plugin-scoped-windows-path\\lib\\plugin.js'
     ];
 
-    let target = {
-      options: {
-        babel: {
-          plugins: [shortNamePlugin, normalizedNamePlugin, fullPathPlugin, scopedPathPlugin, scopedWindowsPathPlugin]
-        }
-      }
-    };
+    let config = [shortNamePlugin, normalizedNamePlugin, fullPathPlugin, scopedPathPlugin, scopedWindowsPathPlugin];
 
     it('locates plugins by short and normalized names', () => {
-      expect(findPlugin(target, 'nonexistent')).to.be.undefined;
+      expect(findPlugin(config, 'nonexistent')).to.be.undefined;
 
-      expect(findPlugin(target, 'short-name')).to.equal(shortNamePlugin);
-      expect(findPlugin(target, 'babel-plugin-short-name')).to.equal(shortNamePlugin);
+      expect(findPlugin(config, 'short-name')).to.equal(shortNamePlugin);
+      expect(findPlugin(config, 'babel-plugin-short-name')).to.equal(shortNamePlugin);
 
-      expect(findPlugin(target, 'long-name')).to.equal(normalizedNamePlugin);
-      expect(findPlugin(target, 'babel-plugin-long-name')).to.equal(normalizedNamePlugin);
+      expect(findPlugin(config, 'long-name')).to.equal(normalizedNamePlugin);
+      expect(findPlugin(config, 'babel-plugin-long-name')).to.equal(normalizedNamePlugin);
 
-      expect(findPlugin(target, 'full-path')).to.equal(fullPathPlugin);
-      expect(findPlugin(target, 'babel-plugin-full-path')).to.equal(fullPathPlugin);
+      expect(findPlugin(config, 'full-path')).to.equal(fullPathPlugin);
+      expect(findPlugin(config, 'babel-plugin-full-path')).to.equal(fullPathPlugin);
 
-      expect(findPlugin(target, '@scope/scoped-path')).to.equal(scopedPathPlugin);
-      expect(findPlugin(target, '@scope/babel-plugin-scoped-path')).to.equal(scopedPathPlugin);
+      expect(findPlugin(config, '@scope/scoped-path')).to.equal(scopedPathPlugin);
+      expect(findPlugin(config, '@scope/babel-plugin-scoped-path')).to.equal(scopedPathPlugin);
 
-      expect(findPlugin(target, '@scope/scoped-windows-path')).to.equal(scopedWindowsPathPlugin);
-      expect(findPlugin(target, '@scope/babel-plugin-scoped-windows-path')).to.equal(scopedWindowsPathPlugin);
+      expect(findPlugin(config, '@scope/scoped-windows-path')).to.equal(scopedWindowsPathPlugin);
+      expect(findPlugin(config, '@scope/babel-plugin-scoped-windows-path')).to.equal(scopedWindowsPathPlugin);
     });
   });
 
   describe('addPlugin', () => {
-    it('creates configuration when necessary', () => {
-      let target = {};
+    describe('with an EmberApp or Addon target', () => {
+      it('creates nested configuration when necessary', () => {
+        let target = {};
 
-      addPlugin(target, 'babel-plugin-foo');
+        addPlugin(target, 'babel-plugin-foo');
 
-      expect(target).to.deep.equal({
-        options: {
-          babel: {
-            plugins: ['babel-plugin-foo']
+        expect(target).to.deep.equal({
+          options: {
+            babel: {
+              plugins: ['babel-plugin-foo']
+            }
           }
-        }
+        });
       });
-    });
 
-    it('leaves existing configuration alone', () => {
-      let target = {
-        name: 'target',
-        options: {
-          otherAddon: true,
-          babel: {
-            loose: true,
-            plugins: ['a', 'b', 'c']
+      it('leaves existing configuration alone', () => {
+        let target = {
+          name: 'target',
+          options: {
+            otherAddon: true,
+            babel: {
+              loose: true,
+              plugins: ['a', 'b', 'c']
+            }
           }
-        }
-      };
+        };
 
-      addPlugin(target, 'd');
+        addPlugin(target, 'd');
 
-      expect(target).to.deep.equal({
-        name: 'target',
-        options: {
-          otherAddon: true,
-          babel: {
-            loose: true,
-            plugins: ['a', 'b', 'c', 'd']
+        expect(target).to.deep.equal({
+          name: 'target',
+          options: {
+            otherAddon: true,
+            babel: {
+              loose: true,
+              plugins: ['a', 'b', 'c', 'd']
+            }
           }
-        }
+        });
       });
     });
 
     it('honors `before` configuration', () => {
-      let target = makeTarget(['a', 'b', 'c', 'd', 'e']);
+      let config = ['a', 'b', 'c', 'd', 'e'];
 
-      addPlugin(target, 'babel-plugin-foo', { before: ['d', 'b', 'c'] });
+      addPlugin(config, 'babel-plugin-foo', { before: ['d', 'b', 'c'] });
 
-      expect(target.options.babel.plugins).to.deep.equal(['a', 'babel-plugin-foo', 'b', 'c', 'd', 'e']);
+      expect(config).to.deep.equal(['a', 'babel-plugin-foo', 'b', 'c', 'd', 'e']);
     });
 
     it('honors `after` configuration', () => {
-      let target = makeTarget(['a', 'b', 'c', 'd', 'e']);
+      let config = ['a', 'b', 'c', 'd', 'e'];
 
-      addPlugin(target, 'babel-plugin-foo', { after: ['b', 'd', 'c'] });
+      addPlugin(config, 'babel-plugin-foo', { after: ['b', 'd', 'c'] });
 
-      expect(target.options.babel.plugins).to.deep.equal(['a', 'b', 'c', 'd', 'babel-plugin-foo', 'e']);
+      expect(config).to.deep.equal(['a', 'b', 'c', 'd', 'babel-plugin-foo', 'e']);
     });
 
     it('honors combined `before` and `after` configuration', () => {
-      let target = makeTarget(['a', 'b', 'c', 'd', 'e']);
+      let config = ['a', 'b', 'c', 'd', 'e'];
 
-      addPlugin(target, 'babel-plugin-foo', { before: ['c'], after: ['b'] });
+      addPlugin(config, 'babel-plugin-foo', { before: ['c'], after: ['b'] });
 
-      expect(target.options.babel.plugins).to.deep.equal(['a', 'b', 'babel-plugin-foo', 'c', 'd', 'e']);
+      expect(config).to.deep.equal(['a', 'b', 'babel-plugin-foo', 'c', 'd', 'e']);
     });
 
     it('ignores `before` and `after` configuration for plugins that are not present', () => {});
 
     it('rejects unsatisfiable `before`/`after` constraints', () => {
-      let target = makeTarget(['a', 'b', 'c', 'd', 'e']);
+      let config = ['a', 'b', 'c', 'd', 'e'];
 
-      expect(() => addPlugin(target, 'babel-plugin-foo', { before: ['c'], after: ['d'] })).to.throw(
+      expect(() => addPlugin(config, 'babel-plugin-foo', { before: ['c'], after: ['d'] })).to.throw(
         'Unable to satisfy placement constraints for Babel plugin babel-plugin-foo'
       );
     });
   });
-
-  function makeTarget(plugins: BabelPluginConfig[]) {
-    return {
-      options: {
-        babel: {
-          plugins
-        }
-      }
-    };
-  }
 });
